8b1e91dc5c245b41bf9e2ac74f1a792e05542295
mreilly29 <mreilly18@jcu.edu>
2018-09-21 14:36:28 -0400

    first draft

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,41 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            bool caredFor = true;
+            VirtualPet dog = new VirtualPet();
+            Console.WriteLine("What is your pet's name?");
+            dog.Name = Console.ReadLine();
+            while (caredFor == true)
+            {
+                dog.Menu();
+                int input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case (1):
+                        dog.Feed();
+                        break;
+                    case (2):
+                        dog.Drink();
+                        break;
+                    case (3):
+                        dog.Walk();
+                        break;
+                    case (4):
+                        dog.Cuddle();
+                        break;
+                    case (5):
+                        caredFor = false;
+                        break;
+                    default:
+                        break;
+                }
+
+
+                if(dog.Hunger == 0 || dog.Thirst == 0)
+                {
+                    caredFor = false;
+                }
+            }
         }
     }
 }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //FIELDS
+        private int hunger = 30;
+        private int thirst = 30;
+        private int waste = 0;
+        private int exercise = 30;
+        private int affection = 30;
+        private string name;
+
+        //PROPERTIES
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+
+        public int Exercise
+        {
+            get { return this.exercise; }
+            set { this.exercise = value; }
+        }
+
+        public int Affection
+        {
+            get { return this.affection; }
+            set { this.affection = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //CONSTRUCTORS
+        public VirtualPet()
+        {
+            //defualt constructor
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("{0} the dog", Name);
+            Console.WriteLine("Hunger: {0}", Hunger);
+            Console.WriteLine("Thirst: {0}", Thirst);
+            Console.WriteLine("Waste: {0}", Waste);
+            Console.WriteLine("Exercise: {0}", Exercise);
+            Console.WriteLine("Affection: {0}", Affection);
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Feed {0}", Name );
+            Console.WriteLine("2. Water {0}",Name);
+            Console.WriteLine("3. Walk {0}", Name);
+            Console.WriteLine("4. Cuddle with {0}", Name);
+            Console.WriteLine("5. Abandon {0}", Name);
+
+        }
+
+        //public void Tick()
+        //{
+
+        //}
+
+        public void Feed()
+        {
+            Hunger += 5;
+            Thirst -= 3;
+            Waste += 2;
+            Exercise -= 3;
+            Console.WriteLine("You fed {0}.", Name);
+            Console.WriteLine();
+        }
+        public void Drink()
+        {
+            Thirst += 5;
+            Waste += 2;
+            Console.WriteLine("You gave {0} water.", Name);
+            Console.WriteLine();
+        }       
+        public void Walk()
+        {
+            Waste -= 5;
+            Exercise += 5;
+            Hunger--;
+            Thirst -= 5;
+            Affection += 2;
+            Console.WriteLine("You walked {0}.", Name);
+            Console.WriteLine();
+        }
+        public void Cuddle()
+        {
+            Affection += 6;
+            Hunger--;
+            Thirst--;
+            Waste--;
+            Console.WriteLine("You cuddled with {0}.", Name);
+            Console.WriteLine();
+        }
+    }
+}
